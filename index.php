60f6bf1b64fe030318bcb3b292f612250aa9ccea
Melissa Wong <mellymai@gmail.com>
2017-07-05 16:50:09 -0700

    test edit and add client-specs.pdf

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-// Index.php file for testing, to make sure 
-// git sync works from our servers. 
+// Index.php file for testing, to make sure
+// git sync works from our servers.
 
 // master git repo: https://github.com/se-linde/Gobi-Wild/
 
+//test edit (mel)
