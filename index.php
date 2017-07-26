95db0c1f69f7d855742de78a661650ec963f19bc
Melissa Wong <mellymai@gmail.com>
2017-07-25 22:25:47 -0700

    Adds styling and fixes date format for calendar page/event listings

--- index.php
+++ index.php
@@ -4,14 +4,11 @@
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); // This starts the loop?> 
     
-    <article id="post-<?php the_ID(); ?>" class="post-excerpt">
-    
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <small>Posted on <?php the_time('f j, Y'); ?> by <?php the_author(); ?> in <?php the_category(', '); ?></small>
+    <article>
+        <h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <small class="byline">Posted on <?php the_time('F j, Y'); ?></small>
         
-        <a href="<?php the_permalink(); ?>">
-            <?php the_post_thumbnail('thumbnail'); ?></a>    
-        
+        <div class="blog-text">
         <?php the_excerpt(); ?>
     </article>
     
