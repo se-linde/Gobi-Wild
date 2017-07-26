e076699517415250ee910b4acbf57add4a402ad5
Melissa Wong <mellymai@gmail.com>
2017-07-25 23:30:26 -0700

    Fixes styles for single.php Removes hyperlinked title, increases margin beneath byline

--- single.php
+++ single.php
@@ -1,17 +1,14 @@
 <?php get_header(); ?>
 
-    <!-- begin the text --> 
-
-    <div id="content">
     <?php if (have_posts()) : while (have_posts()) : the_post(); // The loop. ?>
         
-        <h2><a href="<?php the_permalink(); ?>">
-            <?php the_title(); ?></a></h2>    
-
-        <small>Posted in <?php the_category(', '); ?></small>
-        <?php the_post_thumbnail('large'); ?>
-        <?php the_content(''); ?> 
-        <?php endwhile; endif; // ends the loop. ?>
+        <h2 class="post-title"><?php the_title(); ?></h2>
+        <small class="byline">Posted on <?php the_time('F j, Y'); ?></small>
+        <div class="blog-text">
+            <?php the_post_thumbnail('large'); ?>
+            <?php the_content(); ?> 
+        </div><!-- .blog-text -->
+    <?php endwhile; endif; // ends the loop. ?>
         
         <small>single.php</small>   
     </div>
