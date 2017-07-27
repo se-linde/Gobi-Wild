62d0dc9030c9f9eeb4c5a9ea02fdb35cc01e604d
Melissa Wong <mellymai@gmail.com>
2017-07-26 19:43:50 -0700

    Changes single.php to adhere to current wireframe

--- single.php
+++ single.php
@@ -2,14 +2,11 @@
 
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         
-        <h2 class="post-title"><?php the_title(); ?> - <?php echo get_post_meta($post->ID, 'date', true); ?></h2>
-        <h3 class="post-meta"><?php echo get_post_meta($post->ID, 'location', true); ?>, <?php echo get_post_meta($post->ID, 'time', true) ?></h3>
-        <div class="blog-text">
-            <?php the_post_thumbnail('large'); ?>
-            <?php the_content(); ?> 
-        </div><!-- .blog-text -->
+        <h1 class="post-title"><?php the_title(); ?></h1>
+        <?php the_content(); ?> 
+
     <?php endwhile; endif; ?>
         
-        <small>single.php</small>   
+    <small>single.php</small>   
         
 <?php get_footer(); ?>
