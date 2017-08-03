95563eab648435930fccd6cd128af748200bf1a5
Melissa Wong <mellymai@gmail.com>
2017-08-02 22:06:27 -0700

    Organizes files

--- front-page.php
+++ front-page.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
+<!-- begin Flexslider --> 
+
+    <div class="flexslider">
+        <ul class="slides">
+        <li><img src="<?php bloginfo('template_directory'); ?>/images/540-400.png" width="940" height="400" alt="Image One"></li>
+        <li><img src="<?php bloginfo('template_directory'); ?>/images/540-400.png" width="940" height="400" alt="Image Two"></li>
+        <li><img src="<?php bloginfo('template_directory'); ?>/images/540-400.png" width="940" height="400" alt="Image Three"></li>
+        </ul>
+</div>
+
+<!-- end Flexslider --> 
+
     <?php 
     if ( have_posts() ) : while ( have_posts() ) : the_post(); 
     	
