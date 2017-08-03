7d973f808306ba6fea3b0ade682b79efb3e254eb
Dwayne Linde <lindesara@gmail.com>
2017-08-02 18:02:01 -0700

    checkin.
    checkin - starting flex slider work

--- front-page.php
+++ front-page.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
+<!-- begin Flexslider --> 
+
+    <div class="flexslider">
+        <ul class="slides">
+        <li><img src="http://http://www.lindese.com/gobiwild/wordpress/templates/images/img-slide-01.jpg" width="540" height="400" alt="Image One"></li>
+        <li><img src="http://http://www.lindese.com/gobiwild/wordpress/templates/images/img-slide-02.jpg" width="540" height="400" alt="Image Two"></li>
+        <li><img src="http://http://www.lindese.com/gobiwild/wordpress/templates/images/img-slide-03.jpg" width="540" height="400" alt="Image Three"></li>
+        </ul>
+    </div>
+
+<!-- end Flexslider --> 
+
     <?php 
     if ( have_posts() ) : while ( have_posts() ) : the_post(); 
     	
