09246ecae602758077b17a551557864b11a12c0c
Melissa Wong <mellymai@gmail.com>
2017-08-09 23:22:59 -0700

    Adds styling and removes unused code from 404.php

--- 404.php
+++ 404.php
@@ -1,21 +1,6 @@
 <?php get_header(); ?>
-        </nav>
-        <!-- header end  -->   
-        
-		<!-- begin content  -->
-     	<div class="background">
-        <!-- Begin text ---->
-        <div class="journal-section">
-  
-       
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<?php get_search_form(); ?>
-        
-        <p>There doesn't seem to be anything here!</p>    
+     
+		<h1>Whoops!</h1>
+        <h2>Page not found, care to try again?</h2>    
             
-        <!-- end content  -->
-        
-        <!-- footer start -->
-        </div>
-        <!-- end text -->
 <?php get_footer(); ?>
